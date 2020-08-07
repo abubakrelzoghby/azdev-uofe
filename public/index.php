bba0de49e748b8eef8893e8c9d5b9a86ec581bb3
Abubakr Elzoghby <abubakr.z@gmail.com>
2020-08-08 02:31:35 +0300

    check env first

--- public/index.php
+++ public/index.php
@@ -1,11 +1,16 @@
 <?php
-require_once __DIR__ . '/../app/env.php';
+$envFile = __DIR__ . '/../app/env.php';
+if(file_exists($envFile)) {
+	require_once $envFile;
+} else {
+	echo "Please copy app/env.example.php to app/env.php and add your values.";
+	exit;
+}
 
 $autoloadFile = __DIR__ . '/../vendor/autoload.php';
 if(file_exists($autoloadFile)) {
 	require_once $autoloadFile;
 	Ofe\onlyFE::run();
 }
-
 echo "Please run <code>composer install</code>";
 exit;
