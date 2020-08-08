1e6e7698ceaae7c04a5fe0df6bf21c72317776b0
Abubakr Elzoghby <abubakr.z@gmail.com>
2020-08-08 03:40:30 +0300

    update composer.json

--- public/index.php
+++ public/index.php
@@ -1,12 +1,4 @@
 <?php
-$envFile = __DIR__ . '/../app/env.php';
-if(file_exists($envFile)) {
-	require_once $envFile;
-} else {
-	echo "Please copy app/env.example.php to app/env.php and add your values.";
-	exit;
-}
-
 $autoloadFile = __DIR__ . '/../vendor/autoload.php';
 if(file_exists($autoloadFile)) {
 	require_once $autoloadFile;
